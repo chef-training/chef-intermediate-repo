9b60840240c79428f7868d1aa6da246a6e976ff2
Franklin Webber <franklin.webber@gmail.com>
2017-10-26 09:57:16 -0500

    Fixes the spec to specify a platform and kitchen info
    Signed-off-by: Franklin Webber <franklin@chef.io>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,9 +7,9 @@
 require 'spec_helper'
 
 describe 'apache::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'When all attributes are default, on an CentOS 6.7' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '6.7')
       runner.converge(described_recipe)
     end
 
