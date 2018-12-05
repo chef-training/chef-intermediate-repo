7416eb7be3dac052bb893b990b620f908260bb64
johnfitzpatrick <jhnftzptrck@gmail.com>
2018-12-05 11:45:21 +0000

    Added 'cookbooks' directory, and apache cookbook within it

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
--- recipes/default.rb
+++
@@ -1,5 +0,0 @@
-#
-# Cookbook:: apache
-# Recipe:: default
-#
-# Copyright:: 2018, The Authors, All Rights Reserved.
